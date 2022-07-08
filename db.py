22860dd9c5f45e5d5f4405a2aa824edd028d8b9a
S0m3B0dYyy <108902105+S0m3B0dYyy@users.noreply.github.com>
2022-07-08 11:15:30 +0300

    Update db.py

--- db.py
+++ db.py
@@ -15,6 +15,12 @@ def check_db():
         cursor.execute("CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INT, nickname TEXT, reg_date TEXT, ref_id INT, balance INT)")
         db.commit()
         print("----   Database was create   ---")
+    try:
+        cursor.execute("SELECT * FROM settings")
+    except sqlite3.OperationalError:
+        cursor.execute("CREATE TABLE settings(id INTEGER PRIMARY KEY AUTOINCREMENT, qiwi INT, video INT, photo INT, stbal INT, bonus INT)")
+        cursor.execute(f"INSERT INTO settings(qiwi, video, photo, stbal, bonus) VALUES (89876543210, 10, 5, 30, 30)")
+        db.commit()
     print(f"-----   {_datetime}   -----")
     print(f"---------   Users: {len(get_all_users())}   --------\n")
 
@@ -29,6 +35,25 @@ def add_user_to_db(user_id, nickname, ref_id, balance):
     cursor = db.cursor()
     if not (cursor.execute(f"SELECT user_id FROM users WHERE user_id = '{user_id}'").fetchone()):
         cursor.execute(f"INSERT INTO users(user_id, nickname, reg_date, ref_id, balance) VALUES ({user_id}, '{nickname}', '{get_now_date()}', {ref_id}, {balance})")
+    db.commit()
+
+def update_nickname(user_id, nickname):
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    cursor.execute(f"UPDATE users SET nickname = '{nickname}' WHERE user_id = {user_id}")
+    db.commit()
+
+def get_settings():
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    cursor.execute(f"SELECT * FROM settings")
+    row = cursor.fetchone()
+    return row
+
+def update_settings(command, value):
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    cursor.execute(f"UPDATE settings SET {command} = {value}")
     db.commit()
 
 def get_users_exist(user_id):
@@ -57,7 +82,13 @@ def get_balance(user_id):
 def set_balance(user_id, balance):
     db = sqlite3.connect('database.db')
     cursor = db.cursor()
-    cursor.execute(f"UPDATE users SET balance = {balance} WHERE user_id IS {user_id}")
+    cursor.execute(f"UPDATE users SET balance = {balance} WHERE user_id = {user_id}")
+    db.commit()
+
+def set_balance_nickname(nickname, balance):
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    cursor.execute(f"UPDATE users SET balance = {balance} WHERE nickname = '{nickname}'")
     db.commit()
 
 def get_refs(user_id):
