05aa1481b571e799994010881170ce802c69e41f
S0m3B0dYyy <108902105+S0m3B0dYyy@users.noreply.github.com>
2022-08-22 22:40:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -302,9 +302,13 @@ async def admin_mail(message: types.Message, state: FSMContext):
 			_text = _text + f"\n{i[5]} | {i[1]} (@{i[2]})"
 		_text = _text + "\n\n"
 		_text = _text + "<b>👥 Топ по рефералам</b>"
-		for i in db.get_top_ref(5):
-			_temp_name = db.get_info(i[0])[2]
-			_text = _text + f"\n{i[1]} | {i[0]} (@{_temp_name})"
+		top_refs = db.get_top_ref(5)
+		if top_refs:
+			for i in top_refs:
+				_temp_name = db.get_info(i[0])[2]
+				_text = _text + f"\n{i[1]} | {i[0]} (@{_temp_name})"
+		else:
+			_text = _text + f"\nНикто никого не пригласил"
 		await message.answer(_text, reply_markup=reply_keyboard(), parse_mode="HTML")
 
 @dp.message_handler(commands="pay", state="*")
